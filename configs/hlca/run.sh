681026b00de6c342f18cb69972801b256b1129a7
Michaela Mueller <michaela.mueller@helmholtz-munich.de>
2025-07-26 14:44:29 -0700

    set up data loading and QC workflow for HLCA

---
+++ configs/hlca/run.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+set -e -x
+
+pipeline="$(realpath ../sc-atlasing-toolbox)"
+
+snakemake \
+  --profile .profiles/czbiohub \
+  --configfile \
+    configs/comp_resources.yaml  \
+    configs/hlca/load_data.yaml \
+    configs/hlca/qc.yaml \
+  --snakefile $pipeline/workflow/Snakefile \
+    $@
