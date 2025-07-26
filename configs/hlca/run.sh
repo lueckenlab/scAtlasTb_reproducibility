740d09517a79b7eaa8ad0211eb01af6702a0281f
Michaela Mueller <michaela.mueller@helmholtz-munich.de>
2025-07-26 15:27:17 -0700

    add cross-study QC and refactor QC config

--- configs/hlca/run.sh
+++ configs/hlca/run.sh
@@ -7,6 +7,7 @@ snakemake \
   --profile .profiles/czbiohub \
   --configfile \
     configs/comp_resources.yaml  \
+    configs/hlca/defaults.yaml  \
     configs/hlca/load_data.yaml \
     configs/hlca/qc.yaml \
   --snakefile $pipeline/workflow/Snakefile \
