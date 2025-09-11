63b6ccbd8e81038f11e10e97a16ad63da1311ada
Michaela Mueller <michaela.mueller@helmholtz-munich.de>
2025-09-11 01:55:16 -0700

    set up batch analysis workflow

---
+++ configs/batch_analysis/run.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+set -e -x
+
+pipeline="$(realpath ../scAtlasTb)"
+
+snakemake \
+  --profile .profiles/czbiohub \
+  --configfile \
+    .profiles/czbiohub/cluster_config.yaml \
+    configs/global.yaml  \
+    configs/batch_analysis/config.yaml  \
+  --snakefile $pipeline/workflow/Snakefile \
+    $@
