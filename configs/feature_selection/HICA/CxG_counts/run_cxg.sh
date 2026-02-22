86213c42c1f1965f9ae5cfe1a5065d00ff0225e8
Michaela Mueller <michaela.mueller@helmholtz-munich.de>
2026-02-22 09:07:29 -0800

    add workflows from CxG counts

---
+++ configs/feature_selection/HICA/CxG_counts/run_cxg.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+set -e -x
+
+pipeline="$(realpath ../scAtlasTb)"
+
+snakemake \
+  --profile .profiles/czbiohub \
+  --configfile \
+    .profiles/czbiohub/cluster_config.yaml \
+    configs/global.yaml  \
+    configs/HICA_gene_sets.yaml  \
+    configs/feature_selection/HICA/defaults.yaml  \
+    configs/feature_selection/HICA/workflow_cxg.yaml \
+  --snakefile $pipeline/workflow/Snakefile \
+    $@
